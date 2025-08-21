141a534bd3cd409b09ebe370c509d4409410634d
madhu <avilelimadhu321@gmail.com>
2025-08-21 16:07:57 +0530

    python

---
+++ assignments/controlstaements/three.py
@@ -0,0 +1,6 @@
+#Program to check if a number is multiple of 3 not not?
+num=int(input("enter number:"))
+if num%3==0:
+    print("it is divisible by 3")
+else:
+    print("it is not divisible by 3")
