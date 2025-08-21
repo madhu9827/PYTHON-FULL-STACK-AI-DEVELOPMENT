40e94c7bc4c8e7b9f24e929a21197f4513d4bccf
madhu <avilelimadhu321@gmail.com>
2025-08-21 16:49:45 +0530

    python

---
+++ assignments/controlstaements/eleven.py
@@ -0,0 +1,7 @@
+#.Program to print the least/small number is given two numbers?
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+if a<b:
+    print("small number is ",a)
+else:
+    print("small number is",b)
