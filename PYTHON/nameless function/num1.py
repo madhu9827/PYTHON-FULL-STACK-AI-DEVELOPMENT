c31c0b900ef91591cc13810212fa11ee079e8a18
madhu <avilelimadhu321@gmail.com>
2025-09-11 19:36:45 +0530

    nameless

---
+++ PYTHON/nameless function/num1.py
@@ -0,0 +1,3 @@
+words = ["apple", "banana", "kiwi", "mango"]
+x=map(lambda w:w.upper(),words)
+print(list(x))
---
+++ PYTHON/nameless function/num4.py
@@ -0,0 +1,3 @@
+names = ["madhu", "sonia", "rahul", "modi", "amith"]
+x=filter(lambda name:len(name)>5,names)
+print(list(x))
