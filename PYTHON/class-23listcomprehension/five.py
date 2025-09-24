7d4ac6d41ca234160700982ef357cdc06a9fe38d
madhu <avilelimadhu321@gmail.com>
2025-09-24 11:57:24 +0530

    static web

---
+++ PYTHON/class-23listcomprehension/five.py
@@ -0,0 +1,15 @@
+class Account:
+    def open_ac(self):
+        print("account opened")
+    def deposit(self):
+        print("amount deposited")
+    def withdraw(self):
+        print("amount withdraw")
+    def get_bal(self):
+        print("available balance")
+a1=Account()
+a2=Account()
+a1.open_ac()
+a1.deposit()
+a1.withdraw()
+a1.get_bal()
--- PYTHON/class-23listcomprehension/four.py
+++ PYTHON/class-23listcomprehension/four.py
@@ -2,5 +2,7 @@ class Account:
     pass
 a1=Account()
 a2=Account()
+a3=Account()
 print(id(a1))
 print(id(a2))
+print(id(a3))
