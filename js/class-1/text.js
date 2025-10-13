ff4e8b29d8869945b54a2520859faeae3927e331
madhu <avilelimadhu321@gmail.com>
2025-10-13 11:04:45 +0530

    js structure

---
+++ js/class-1/text.js
@@ -0,0 +1,9 @@
+// console.log("hello world!");
+// document.writeln("hello world!")
+var eid=101;
+var ename="madhu"
+var esal=45000.45
+var avail=false;
+console.log(eid);
+console.log(ename);
+document.writeln(esal);
