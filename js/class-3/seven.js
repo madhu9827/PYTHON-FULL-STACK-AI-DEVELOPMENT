94c060f758811181ff13785af640e9f08113dac1
madhu <avilelimadhu321@gmail.com>
2025-10-15 16:24:29 +0530

    js datatypes

---
+++ js/class-3/seven.js
@@ -0,0 +1,17 @@
+//js-object:group of key pair values
+//create object
+let a={
+    "uid":101,
+    "uname":"prasanna"
+
+}
+//read
+console.log(a)
+//update
+a.loc="bangalore"
+console.log(a)
+a.sal=29000;
+console.log(a)
+//del
+delete a.sal;
+console.log(a)
---
+++ js/class-3/six.js
@@ -0,0 +1,15 @@
+//js-array:group of elements as one entity
+//create array
+let e=["madhu","prasannna","srinithi","lali","tanuj"]
+console.log(e)//read
+console.log(e[0])
+console.log(e[3])
+console.log(e[1])
+console.log(e[4])
+console.log(typeof(e))
+//update
+e[5]="bhavani"
+console.log(e)
+//delete
+delete e[3];
+console.log(e)
