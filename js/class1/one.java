9a6c86659482d26ba2f8fcb790ce8b6eddd873e2
madhu <avilelimadhu321@gmail.com>
2025-10-24 15:15:44 +0530

    js-introduction

---
+++ js/class1/one.java
@@ -0,0 +1,9 @@
+class test{
+    public static void main(String[] args){
+    int eid=101;
+    String ename="Rahul";
+    double esal=45000.45;
+    System.out.println(eid);
+    System.out.println(ename);
+    }
+}
---
+++ js/class1/two.js
@@ -0,0 +1,4 @@
+let a=10
+let b=10.5
+//document.writeln(a+b)
+console.log(a+b)
