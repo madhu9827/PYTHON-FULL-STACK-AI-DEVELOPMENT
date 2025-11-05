c48d236f7d79d8c866b5acb749399ab61ae8d7ce
madhu <avilelimadhu321@gmail.com>
2025-11-05 17:15:49 +0530

    file handling

---
+++ PYTHON/class17-inner function/3.py
@@ -0,0 +1,9 @@
+def calc():
+    def add():
+        print('addition')
+    def mul():
+        print('multiplication')
+    return 100,200
+    # return mul
+inn=calc()
+print(inn)
---
+++ PYTHON/class17-inner function/one1.py
@@ -0,0 +1,11 @@
+def calc():
+    print("calculation")
+    def add():
+        print("addition")
+    add()
+    add()
+    def multi():
+        print("multiplication")
+    multi()
+calc()
+
